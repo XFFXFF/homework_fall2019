17d4b567c5158c8e7d05dd2939d12c95496ee3e0
XFFXFF <1247714429@qq.com>
2019-10-13 13:42:45 +0800

    bringing in the required components from the previous homework

--- hw2/cs285/infrastructure/utils.py
+++ hw2/cs285/infrastructure/utils.py
@@ -7,7 +7,7 @@ import time
 def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
 
     # initialize env for the beginning of a new rollout
-    ob = TODO # TODO: GETTHIS from HW1
+    ob = env.reset() # TODO: GETTHIS from HW1
 
     # init vars
     obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
@@ -30,7 +30,7 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 
         # use the most recent ob to decide what to do
         obs.append(ob)
-        ac = TODO # TODO: GETTHIS from HW1
+        ac = policy.get_action(ob) # TODO: GETTHIS from HW1
         ac = ac[0]
         acs.append(ac)
 
@@ -44,7 +44,7 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 
         # End the rollout if the rollout ended 
         # Note that the rollout can end due to done, or due to max_path_length
-        rollout_done = TODO # TODO: GETTHIS from HW1
+        rollout_done = done or (steps > max_path_length) # TODO: GETTHIS from HW1
         terminals.append(rollout_done)
         
         if rollout_done: 
@@ -55,12 +55,22 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
 
     # TODO: GETTHIS from HW1
+    timesteps_this_batch = 0
+    paths = []
+    while timesteps_this_batch < min_timesteps_per_batch:
+        path = sample_trajectory(env, policy, max_path_length, render=render, render_mode=render_mode)
+        timesteps_this_batch += get_pathlength(path)
+        paths.append(path)
 
     return paths, timesteps_this_batch
 
 def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, render_mode=('rgb_array')):
     
     # TODO: GETTHIS from HW1
+    paths = []
+    for _ in range(ntraj):
+        path = sample_trajectory(env, policy, max_path_length, render=render, render_mode=render_mode)
+        paths.append(path)
 
     return paths
 
